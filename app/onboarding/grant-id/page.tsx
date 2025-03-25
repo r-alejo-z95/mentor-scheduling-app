4f55970133bbf4afb7a286da1a0829f4894f6d78
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-03-24 23:31:41 -0500

    Add onboarding page for calendar connection

---
+++ app/onboarding/grant-id/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
+export default function OnboradingRouteNylas() {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center bg-slate-100">
+      <Card className="min-w-[450px] md:min-w-[550px] p-6 shadow-lg rounded-lg mx-auto">
+        <CardHeader>
+          <CardTitle className="text-lg md:text-2xl font-bold md:text-center">
+            You are almost done!
+          </CardTitle>
+          <CardDescription className="text-xs md:text-lg md:text-center mb-2">
+            We just need to connect to your calendar
+          </CardDescription>
+          <div className="flex justify-center">
+            <img
+              src="https://media1.tenor.com/m/hrj3t9pi5LoAAAAC/despicable-me-gru.gif"
+              alt="Almost done gif"
+              width={10}
+              height={10}
+              className="w-full justify-self-center rounded-md shadow-lg p-1 bg-orange-200"
+            />
+          </div>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+}
